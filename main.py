dcf9bae76e68217bf15702469ce815fbdcccd954
Xenoxo <timwyc@gmail.com>
2023-05-07 19:06:21 +0200

    Add more styling and minute conversion.

--- main.py
+++ main.py
@@ -3,23 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import statistics
 
-# num = random.random()
-
-# def num_squared (num):
-#     return num ** 2
-
-# print(num)
-
-
-# x = np.linspace(0, 5, 100)
-
-# def f(x):
-#     return np.exp(x)
-
-# plt.plot(x, f(x))
-
-# plt.show()
-
 def get_reward():
     tiers = ["Common", "Uncommon", "Rare", "Epic", "Legendary"]
     probabilities = [0.6, 0.25, 0.1, 0.04, 0.01]
@@ -30,6 +13,7 @@ def get_reward():
     BLUE = '\033[34m'
     PURPLE = '\033[35m'
     RED = '\033[31m'
+    UNDERLINE = '\033[4m'
     RESET = '\033[0m'
     reward = 0
     if rarity == "Common":
@@ -47,7 +31,7 @@ def get_reward():
     elif rarity == "Legendary":
         reward = 10800
         color = RED
-    formatted_rarity = color + rarity.upper() + RESET
+    formatted_rarity = UNDERLINE + color + rarity.upper() + RESET
     return reward, formatted_rarity
 
 print("\n\nWelcome to the rewards machine! Would you like to spin the wheel? (y/n)")
@@ -56,11 +40,12 @@ response = input()
 keep_playing = True
 while response != 'n':
     reward, formatted_rarity = get_reward()
-    print(f"\nYou pulled a {formatted_rarity} piece of loot! You are rewarded with {reward} seconds!")
+    minutes = reward // 60
+    print(f"\nYou pulled a {formatted_rarity} piece of loot! You are rewarded with \033[4m{minutes}\033[0m minutes!")
     print("Go again? (Press any key to go again, type \"n\" to end the game)")
     response = input().lower()
 
-print("Thanks for playing ya bub!")
+print("\nThanks for playing ya bub!")
 
 
 # print('\033[32m' + 'This text is green!' + '\033[0m')
@@ -73,3 +58,8 @@ print("Thanks for playing ya bub!")
 # results = [get_reward()[0] for _ in range(10000)]
 # mean = statistics.mean(results)
 # print(mean)
+
+print("\033[1mThis text is large.\033[0m")
+print("\033[7mThis text is small.\033[0m")
+print("\033[1mThis text is bold.\033[0m")
+print("\033[4mThis text is underlined.\033[0m")

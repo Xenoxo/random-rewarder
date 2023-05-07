d2cd30ce3e444a2c0b23b43b31c4df1b0d63e59f
Xenoxo <timwyc@gmail.com>
2023-05-07 18:49:39 +0200

    Add base reward loop and rarity system

--- main.py
+++ main.py
@@ -20,43 +20,56 @@ import statistics
 
 # plt.show()
 
-def get_seconds():
-    # tiers = [1, 2, 3, 4, 5]
+def get_reward():
     tiers = ["Common", "Uncommon", "Rare", "Epic", "Legendary"]
     probabilities = [0.6, 0.25, 0.1, 0.04, 0.01]
     rarity = random.choices(tiers, weights=probabilities, k=1)[0]
-    seconds = 0
+    color = ''
+    WHITE = '\033[37m'
+    GREEN = '\033[32m'
+    BLUE = '\033[34m'
+    PURPLE = '\033[35m'
+    RED = '\033[31m'
+    RESET = '\033[0m'
+    reward = 0
     if rarity == "Common":
-        seconds = 300
+        reward = 300
+        color = WHITE
     elif rarity == "Uncommon":
-        seconds = 900
+        reward = 900
+        color = GREEN
     elif rarity == "Rare":
-        seconds = 1800
+        reward = 1800
+        color = BLUE  
     elif rarity == "Epic":
-        seconds = 5400
+        reward = 5400
+        color = PURPLE   
     elif rarity == "Legendary":
-        seconds = 10800
-    return seconds, rarity
-
-results = [get_seconds()[0] for _ in range(10000)]
-mean = statistics.mean(results)
-print(mean)
+        reward = 10800
+        color = RED
+    formatted_rarity = color + rarity.upper() + RESET
+    return reward, formatted_rarity
 
 print("\n\nWelcome to the rewards machine! Would you like to spin the wheel? (y/n)")
 
 response = input()
 keep_playing = True
-while keep_playing:
-    seconds, rarity = get_seconds()
-    print(f"You pulled a {rarity} piece of loot! You are rewarded with {seconds} seconds", )
-    print("Would you like to go again? (Press any key to go again, type \"n\" to end the game)")
-    response = input()
-    keep_playing = response.lower() != "n"
- 
+while response != 'n':
+    reward, formatted_rarity = get_reward()
+    print(f"\nYou pulled a {formatted_rarity} piece of loot! You are rewarded with {reward} seconds!")
+    print("Go again? (Press any key to go again, type \"n\" to end the game)")
+    response = input().lower()
+
 print("Thanks for playing ya bub!")
 
-# if response.lower() == "y":
-#     seconds, rarity = get_seconds()
-#     print(f"You pulled a {rarity} piece of loot! You are rewarded with {seconds} seconds", )
-# else:
-#     print("Thanks for playing ya bub!")
+
+# print('\033[32m' + 'This text is green!' + '\033[0m')
+# print('\033[34m' + 'This text is blue!' + '\033[0m')
+# print('\033[35m' + 'This text is purple!' + '\033[0m')
+# print('\033[33m' + 'This text is orange!' + '\033[0m')
+# print('\033[37m' + 'This text is white!' + '\033[0m') 
+
+
+# results = [get_reward()[0] for _ in range(10000)]
+# mean = statistics.mean(results)
+# print(mean)
